ec3211ec5efba802c5dc511ed9b65815fb94e749
Thomas Bridge <tbridge@chicagobooth.edu>
2017-09-24 22:38:36 +0100

    Final version at the end of the acloud.guru course.    More work to continue...

---
+++ __tests__/test-example-work-modal.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWorkModal from '../js/example-work-modal';
+
+const myWork = [
+	{
+		'title': "Work Example",
+		'href': "https://example.com",
+		'desc': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et mi non nisi luctus consequat. Etiam tristique sem sem, id hendrerit nisl dictum non. Nulla porta accumsan mollis. Aliquam interdum in ex ut placerat. Phasellus mattis magna tellus, sit amet porta urna mattis ut. Suspendisse potenti. Mauris lobortis bibendum elit. Sed malesuada justo et neque consectetur venenatis. Curabitur elementum placerat efficitur. Vivamus gravida ultrices sagittis. Aenean tristique risus sit amet enim porta, id facilisis lacus dapibus. Vivamus et vehicula est. Donec vel magna nec risus sodales sodales.",
+		'image': {
+			'desc': "example screenshot of a project involving code",
+			'src': "images/example1.png",
+			'comment': ""
+		}
+	},
+	{
+		'title': "A Serverless Portfolio",
+		'href': "https://www.datapipe.com",
+		'desc': "Duis lectus nibh, ullamcorper quis tortor at, porttitor porta ipsum. Aliquam consequat, velit vel mattis dapibus, leo tortor faucibus diam, eu fermentum tellus ex euismod urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc eget tincidunt est. Ut ac mauris pharetra, ullamcorper ex vitae, tempus enim. Maecenas sed ante nisi. Cras purus orci, maximus eu nulla ut, bibendum pretium lacus. Aliquam erat volutpat. Praesent eu faucibus odio. Aenean imperdiet mauris et odio ultricies pellentesque. Pellentesque sit amet orci vitae velit pellentesque varius id a nisi. Phasellus facilisis nisl sed blandit pulvinar. Sed tempor eu metus iaculis pellentesque.",
+		'image': {
+			'desc': "example screenshot of a project involving chemistry",
+			'src': "images/example2.png",
+			'comment': "I worked here!"
+		}
+	}
+];
+	
+describe("ExampleWorkModal component", () => {
+	let component = shallow(<ExampleWorkModal example={myWork[1]} open={false}/>);
+	let openComponent = shallow(<ExampleWorkModal example={myWork[1]} open={true}/>);
+	
+	let anchors = component.find("a");
+	it("Should contain a single 'a' element", () => {
+		expect(anchors.length).toEqual(1);
+	});
+	it("Should link to our project", () => {
+		expect(anchors.node.props.href).toEqual(myWork[1].href);
+	});
+	
+	it("Should have modal class set correctly", () => {
+		expect(component.find(".background--skyBlue").hasClass('modal--closed')).toBe(true);
+		expect(openComponent.find(".background--skyBlue").hasClass('modal--open')).toBe(true);
+		expect(openComponent.find(".background--skyBlue").hasClass('modal--closed')).toBe(false);
+	});
+});
